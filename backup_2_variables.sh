72bf253095b9359d7adbef18ed9be8def5abbcdc
Junaid Khan <junaidkhandev123@gmail.com>
2022-01-20 21:25:01 -0500

    Exercise on variable

--- backup_2_variables.sh
+++ backup_2_variables.sh
@@ -6,9 +6,9 @@ LOGFILE="/home/$USER/log_file"
 BACKUP_LOC="/usr/bin/*"
 BACKUP_TARGET="/home/$USER/backup"
 
-echo "Creating backup directory" && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists"
+echo "Creating backup directory" && mkdir $BACKUP_TARGET 2 > /dev/null || echo "Directory already exists"
 
-echo "Copying Files" && cp -v $BACKUP_LOC $BACKUP_TARGER > $LOGFILE 2>&1
+echo "Copying Files" && cp -v $BACKUP_LOC $BACKUP_TARGET > $LOGFILE 2>&1
 
 grep -i denied $LOGFILE | tail -n 2
 
