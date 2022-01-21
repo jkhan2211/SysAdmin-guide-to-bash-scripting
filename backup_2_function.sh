8d0fc2a3143e3b70b03dff1f7a0941ed03ec5754
Junaid Khan <junaidkhandev123@gmail.com>
2022-01-20 21:07:45 -0500

    function example

--- backup_2_function.sh
+++ backup_2_function.sh
@@ -2,7 +2,7 @@
 
 # Backing up required files
 
-LOGFILE="/home/$USER/log_file"
+LOGFILE=$1
 BACKUP_LOC="/usr/bin/*"
 BACKUP_TARGET="/home/$USER/backup"
 
@@ -11,9 +11,13 @@ init () {
     echo "Creating backup directory" && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists"
 }
 
+tail () {
+    command tail -n $1
+}
+
 init 
 echo "Copying Files" && cp -v $BACKUP_LOC $BACKUP_TARGER > $LOGFILE 2>&1
 
-grep -i denied $LOGFILE | tail -n 2
+grep -i denied $LOGFILE | tail 2
 
 exit 127
