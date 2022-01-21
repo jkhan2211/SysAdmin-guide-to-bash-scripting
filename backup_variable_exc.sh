ccd6026c42bd3ef725576e92afc4d65bc5f700ea
Junaid Khan <junaidkhandev123@gmail.com>
2022-01-20 21:22:38 -0500

    Exercise on variable

---
+++ backup_variable_exc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Task1: Modify the backup.sh script to use variables
+# - Use the given backup script as the base and modify it to use variable for user directory
+
+#Task2: Add a Parameter to determine the name of the log file
+# - Modify the log file usage to use a command line parameter 
+
+#Task3: Run the script in each user's home directory to ensure it works 
+
