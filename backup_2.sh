b9c32b009bdebccedaa560f4592190bc2b6cc9f9
Junaid Khan <junaidkhandev123@gmail.com>
2022-01-20 20:59:57 -0500

    backup v2

--- backup_2.sh
+++ backup_2.sh
@@ -3,11 +3,13 @@
 # Backing up required files
 
 LOGFILE="/home/$USER/log_file"
+BACKUP_LOC="/usr/bin/*"
+BACKUP_TARGET="/home/$USER/backup"
 
-echo "Creating backup directory" && mkdir ~/backup 2> /dev/null || echo "Directory already exists"
+echo "Creating backup directory" && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists"
 
-echo "Copying Files" && cp -v /usr/bin/* ~/backup > $LOGFILE 2>&1
+echo "Copying Files" && cp -v $BACKUP_LOC $BACKUP_TARGER > $LOGFILE 2>&1
 
-grep -i denied log_file | tail -n 2
+grep -i denied $LOGFILE | tail -n 2
 
 exit 127
