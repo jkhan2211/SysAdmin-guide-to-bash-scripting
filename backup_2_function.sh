21fd4fae4633fada49d23c5b1da7c2f66cd5fd50
Junaid Khan <junaidkhandev123@gmail.com>
2022-01-21 19:08:26 -0500

    script updated

--- backup_2_function.sh
+++ backup_2_function.sh
@@ -2,26 +2,46 @@
 
 # Backing up required files
 
+if [ -z $1 ]
+then
+        echo "You need to supply a parameter for the logfile"
+        exit 255
+fi
+
 LOGFILE=$1
 BACKUP_LOC="/usr/bin/*"
 BACKUP_TARGET="/home/$USER/backup"
 
 #function init {...}
 init () {
-    echo "Creating backup directory" && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists"
-    echo "$(date +"%x %r %Z")" > $LOGFILE
-    local CLOUDGURU=37
+    if [ -d $BACKUP_TARGET ]
+        then
+            echo "Directory already exists"
+            echo "$(date + "%x %r %Z") " >> $LOGFILE
+            return 1
+        else
+            mkdir $BACKUP_TARGET
+            echo "$(date + "%x %r %Z")" >> $LOGFILE
+            return 0
 }
 
 tail () {
     command tail -n $1
 }
+
+
 cleanup () {
     rm -rf $BACKUP_TARGET
     echo "RECIEVED CTRLC" >> /home/$USER/$LOGFILE
+    exit
 }
 
-init
+if (init)
+then
+    echo "Directory did not exist"
+else
+    echo "Directory did exists" 
+    
 trap cleanup SIGINT
 
 #echo "Copying Files" && cp -v $BACKUP_LOC $BACKUP_TARGET >> $LOGFILE 2>&1
